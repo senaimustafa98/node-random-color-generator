95c570e620287cfe5853b14979690b9f0319f09f
Senay Mustafa <senaim98@gmail.com>
2024-09-20 18:05:46 +0200

    Fixing coloring of the # rather than the background

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ ${borderColor('#'.repeat(34))}
 ${borderColor('#'.repeat(34))}
 ${borderColor('#'.repeat(34))}
 ${borderColor('#'.repeat(3)) + ' '.repeat(28) + borderColor('#'.repeat(3))}
-${borderColor('#'.repeat(3)) + ' '.repeat(7) + chalk.bgHex(hexOfColor).white(` ${hexOfColor} `) + ' '.repeat(12) + borderColor('#'.repeat(3))}
+${borderColor('#'.repeat(3)) + ' '.repeat(7) + chalk.hex(hexOfColor)(` ${hexOfColor} `) +' '.repeat(12) + borderColor('#'.repeat(3))}
 ${borderColor('#'.repeat(3)) + ' '.repeat(28) + borderColor('#'.repeat(3))}
 ${borderColor('#'.repeat(34))}
 ${borderColor('#'.repeat(34))}
@@ -20,18 +20,18 @@ if (argv.length < 3) {
   // if no input generating random color
   const randColor = randomColor();
   console.log('No input provided. Generating a random color');
-  const colorChalk = chalk.bgHex(randColor);
+  const colorChalk = chalk.hex(randColor);
   // Coloring the pattern with the random color and HEX
   console.log(createPattern(colorChalk, randColor));
 }
 // accepting user input, starting index 2 for hue and 3 for luminosity
 else {
   const hue = argv[2];
-  const luminosity = argv[3] || 'light';
+  const luminosity = argv[3];
 
   // Generate a HEX color based on user input
   const hexOfColor = randomColor({ luminosity, hue });
-  const borderColor = chalk.bgHex(hexOfColor);
+  const borderColor = chalk.hex(hexOfColor);
   // color the pattern with the generated color and HEX value
   console.log(createPattern(borderColor, hexOfColor));
 }
